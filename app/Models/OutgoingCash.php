d140cab8e598e254312e963187ee45a3ae45c6b9
Riki-Saputra <Rickzzsaputra28@gmail.com>
2022-11-20 13:15:03 +0800

    pengeluaran kas frame

---
+++ app/Models/OutgoingCash.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OutgoingCash extends Model
+{
+    use HasFactory;
+    protected $table = 'outgoing_cash';
+
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2022_11_20_130617_create_outgoing_cash_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOutgoingCashTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outgoing_cash', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('note_number', 50);
+            $table->string('employee_name', 100);
+            $table->string('jabatan', 50);
+            $table->date('outgoing_date');
+            $table->string('total');
+            $table->string('note', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outgoing_cash');
+    }
+}
